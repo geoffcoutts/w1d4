e54591cb30ce24189320a047796afb3d9c282bbd
Geoff Coutts <geoff.coutts@gmail.com>
2018-02-15 20:20:06 +0000

    loaded-die.js using closures

---
+++ loaded-die.js
@@ -0,0 +1,16 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  let count=-1;
+  return function() {
+    /* your code here */
+    count++;
+    return list[count];
+  };
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
